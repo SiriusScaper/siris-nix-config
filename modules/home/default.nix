85b8156d1434d87ef724faca10de3efeca59d03e
SiriusScaper <ruselmann.dev@gmail.com>
2024-04-20 10:00:07 -0700

    reformat for default.nix

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,31 +1,31 @@
-{inputs, username, ...}: {
+{inputs, username, host, ...}: {
   imports = [
   # [(import ./aseprite/aseprite.nix)]         # pixel art editor
   # [(import ./audacious/audacious.nix)]       # music player
-   import ./shell/bat.nix
-   import ./shell/btop.nix
+   ./shell/bat.nix
+   ./shell/btop.nix
   # [(import ./cava.nix)]
-   import ./chat/discord.nix           # discord
-   import ./floorp/floorp.nix            # firefox based browser
-   import ./gaming/gaming.nix
-   (import ./dev/git.nix
+   ./chat/discord.nix           # discord
+   ./floorp/floorp.nix            # firefox based browser
+   ./gaming/gaming.nix
+   ./dev/git.nix
   # [(import ./gtk.nix)]
   # [(import ./hyprland)]
   # [(import ./kitty.nix)]
   # [(import ./mako.nix)]                      # notification deamon
   # [(import ./micro.nix)]                     # nano replacement
   # [(import ./nvim.nix)]
-   import ./obs-studio/obs-studio.nix
-   import ./packages.nix
-   import ./qbittorrent/qbittorrent.nix
+   ./obs-studio/obs-studio.nix
+   ./packages.nix
+   ./qbittorrent/qbittorrent.nix
   # [(import ./scripts/scripts.nix)]           # personal scripts
-   import ./shell/starship.nix
+   ./shell/starship.nix
   # [(import ./swaylock.nix)]
   # [(import ./vscodium.nix)]
-   import ./terminals/warp.nix
+    ./terminals/warp.nix
   # [(import ./waybar)]
   # [(import ./wofi/wofi.nix)]
-   import ./youtube-music/yt-music.nix
-   import ./shell/zsh.nix
+   ./youtube-music/yt-music.nix
+   ./shell/zsh.nix
   ];
 }
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,21 +1,21 @@
 { inputs, nixpkgs, self, username, host, ...}:
 {
   imports = [
-   import ./bootloader.nix
-   import ./desktops/plasma6.nix
-   import ./flatpak.nix
-   import ./hardware.nix
-   import ./xserver.nix
-   import ./network.nix
-   import ./nvidia.nix
-   import ./pipewire.nix
-   import ./program.nix
-   import ./scanners.nix
-   import ./security.nix
-   import ./services.nix
-   import ./system.nix
-   import ./user.nix
-   import ./wayland.nix
+    ./bootloader.nix
+    ./desktops/plasma6.nix
+    ./flatpak.nix
+    ./hardware.nix
+    #./xserver.nix
+    ./network.nix
+    ./nvidia.nix
+    ./pipewire.nix
+    ./program.nix
+    ./scanners.nix
+    ./security.nix
+    ./services.nix
+    ./system.nix
+    ./user.nix
+    ./wayland.nix
    #[ (import ./cloudflare-warp.nix) ]
    #[ (import ./virtualization.nix) ];
   ];
