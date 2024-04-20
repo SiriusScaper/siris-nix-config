08cc8de617f9354dad1c067de886960c1a0387d2
SiriusScaper <ruselmann.dev@gmail.com>
2024-04-20 02:42:13 -0700

    restructured dirs and host nix files

---
+++ hosts/homedesktop/default.nix
@@ -0,0 +1,89 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+/*
+{ config, pkgs, inputs, lib, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
+    ];
+
+
+  # Set a Default DM
+  services.xserver.displayManager.defaultSession = "plasma";
+
+  
+  # Enable flatpak support
+  services.flatpak.enable = true;
+   
+
+
+  home-manager = {
+    # also pass inputs to home-manager modules
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      "sirius" = import ./home.nix;
+    };
+  };
+
+
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+}*/
+
+
+{
+  description = "SiriusScaper's nixos configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nur.url = "github:nix-community/NUR";
+
+    hypr-contrib.url = "github:hyprwm/contrib";
+    hyprpicker.url = "github:hyprwm/hyprpicker";
+
+    alejandra.url = "github:kamadorueda/alejandra/3.0.0";
+
+    nix-gaming.url = "github:fufexan/nix-gaming";
+
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, self, ...} @ inputs:
+  let
+    selfPkgs = import ./pkgs;
+    username = "sirius";
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+    lib = nixpkgs.lib;
+  in
+  {
+    overlays.default = selfPkgs.overlay;
+    nixosConfigurations = {
+      desktop = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [ (import ./hosts/desktop) ];
+        specialArgs = { host="desktop"; inherit self inputs username ; };
+      };
+      laptop = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [ (import ./hosts/laptop) ];
+        specialArgs = { host="laptop"; inherit self inputs username ; };
+      };
+    };
+  };
+}
---
+++ modules/home/security/1password.nix
@@ -0,0 +1,4 @@
+{ pkgs, ... }:
+{
+  home.packages = ( with pkgs; [ _1password-gui ]);
+}
