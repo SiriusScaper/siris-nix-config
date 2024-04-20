c35f8fc7be26b24da22e3b987a82b0b3fe254957
SiriusScaper <ruselmann.dev@gmail.com>
2024-04-20 02:44:46 -0700

    fixed a whoopsie with the default.nix and flake.nix files

--- flake.nix
+++ flake.nix
@@ -1,28 +1,81 @@
+# {
+#   description = "Nixos config flake";
+#
+#   inputs = {
+#     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+#     #nur.url = "github:nix-community/NUR";
+#
+#       home-manager = {
+#         url = "github:nix-community/home-manager";
+#         inputs.nixpkgs.follows = "nixpkgs";
+#       };
+#   };
+#
+#   outputs = { self, nixpkgs, ... }@inputs:
+#     let
+#       system = "x86_64-linux";
+#       pkgs = nixpkgs.legacyPackages.${system};
+#     in
+#     {
+#      nixosConfigurations.FarScape-One = nixpkgs.lib.nixosSystem {
+#       specialArgs = {inherit inputs;};
+#       modules = [
+#         ./hosts/homedesktop/configuration.nix
+#         inputs.home-manager.nixosModules.default
+#       ];
+#     };
+#   };
+# }
+
 {
-  description = "Nixos config flake";
+  description = "SiriusScaper's nixos configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    #nur.url = "github:nix-community/NUR";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nur.url = "github:nix-community/NUR";
 
-      home-manager = {
-        url = "github:nix-community/home-manager";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
+    hypr-contrib.url = "github:hyprwm/contrib";
+    hyprpicker.url = "github:hyprwm/hyprpicker";
+
+    alejandra.url = "github:kamadorueda/alejandra/3.0.0";
+
+    nix-gaming.url = "github:fufexan/nix-gaming";
+
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: 
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-     nixosConfigurations.FarScape-One = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./hosts/homedesktop/configuration.nix
-        inputs.home-manager.nixosModules.default
-      ];
+  outputs = { nixpkgs, self, ...} @ inputs:
+  let
+    selfPkgs = import ./pkgs;
+    username = "sirius";
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+    lib = nixpkgs.lib;
+  in
+  {
+    overlays.default = selfPkgs.overlay;
+    nixosConfigurations = {
+      desktop = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [ (import ./hosts/desktop) ];
+        specialArgs = { host="desktop"; inherit self inputs username ; };
+      };
+      laptop = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [ (import ./hosts/laptop) ];
+        specialArgs = { host="laptop"; inherit self inputs username ; };
+      };
     };
   };
 }
