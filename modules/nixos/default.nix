79507b32c72a7f2dea1eb2a4622efa6f3e395859
SiriusScaper <ruselmann.dev@gmail.com>
2024-04-20 03:38:24 -0700

    reformatted default.nix in modules/nixos

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,21 +1,23 @@
 { inputs, nixpkgs, self, username, host, ...}:
 {
-    imports =
-       [ (import ./bootloader.nix) ]
-       [ (import ./desktops/plasma6.nix) ]
-       [ (import ./flatpak.nix) ]
-       [ (import ./hardware.nix) ]
-       [ (import ./xserver.nix) ]
-       [ (import ./network.nix) ]
-       [ (import ./nvidia.nix) ]
-       [ (import ./pipewire.nix) ]
-       [ (import ./program.nix) ]
-       [ (import ./scanners.nix) ]
-       [ (import ./security.nix) ]
-       [ (import ./services.nix) ]
-       [ (import ./system.nix) ]
-       [ (import ./user.nix) ]
-       [ (import ./wayland.nix) ];
+  imports = [
+   import ./bootloader.nix
+   import ./desktops/plasma6.nix
+   import ./flatpak.nix
+   import ./hardware.nix
+   import ./xserver.nix
+   import ./network.nix
+   import ./nvidia.nix
+   import ./pipewire.nix
+   import ./program.nix
+   import ./scanners.nix
+   import ./security.nix
+   import ./services.nix
+   import ./system.nix
+   import ./user.nix
+   import ./wayland.nix
+
+  ];
 #        [ (import ./cloudflare-warp.nix) ]
 #        [ (import ./virtualization.nix) ];
 }
