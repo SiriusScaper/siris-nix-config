36222876c09f1fd8543cda88ba046b9f4341ae72
SiriusScaper <ruselmann.dev@gmail.com>
2024-04-28 13:23:49 -0700

    adjusting laptop module

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -1,21 +1,9 @@
-#    nixpkgs.url = "nixpkgs/nixos-unstable";
-#     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-#
-#    nixosConfigurations = {
-#      tardis = lib.nixosSystem {
-#        inherit system;
-#        modules = [
-# 	./configuration.nix
-# 	nixos-hardware.nixosModules.framework-13-7040-amd
-# 	];
-#       };
-
 { pkgs, config, ... }: 
 {
   imports = [
     ./hardware-configuration.nix
     ./../../modules/system
-#     nixos-hardware.nixosModules.framework-13-7040-amd
+     nixos-hardware.nixosModules.framework-13-7040-amd
   ];
 
    environment.systemPackages = with pkgs; [
@@ -32,37 +20,4 @@
      power-profiles-daemon.enable = true;
      fwupd.enable = true;
    };
-#
-#     upower = {
-#       enable = true;
-#       percentageLow = 20;
-#       percentageCritical = 5;
-#       percentageAction = 3;
-#       criticalPowerAction = "PowerOff";
-#     };
-#
-#     auto-cpufreq = {
-#       enable = true;
-#       settings = {
-#         battery = {
-#           governor = "performance";
-#           turbo = "auto";
-#         };
-#         charger = {
-#           governor = "performance";
-#           turbo = "auto";
-#         };
-#       };
-#     };
-#   };
-#
-#   boot = {
-#     kernelModules = ["acpi_call"];
-#     extraModulePackages = with config.boot.kernelPackages;
-#       [
-#         acpi_call
-#         cpupower
-#       ]
-#       ++ [pkgs.cpupower-gui];
-#   };
  }
