60bf8bb0fe650d2ef61b8e4ac9622c4442f2b0ac
SiriusScaper <ruselmann.dev@gmail.com>
2024-04-28 01:35:27 -0700

    added heroiclauncher and proton vpn to default.nix

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,6 +1,7 @@
 {inputs, username, host, ...}: {
   imports = [
    ./security/1password.nix
+   ./security/protonvpn.nix
   # [(import ./aseprite/aseprite.nix)]         # pixel art editor
   # [(import ./audacious/audacious.nix)]       # music player
    ./shell/bat.nix
@@ -12,10 +13,10 @@
   # [(import ./gtk.nix)]
   # [(import ./hyprland)]
   # [(import ./kitty.nix)]
-  # ./kdeconnect/kdeconnect.nix
   # [(import ./mako.nix)]                      # notification deamon
    ./editors/micro.nix			       # nano replacement
    ./editors/nvim.nix
+   ./gaming/heroic-launcher.nix
    ./obs-studio/obs-studio.nix
    ./packages/packages.nix
    ./qbittorrent/qbittorrent.nix
