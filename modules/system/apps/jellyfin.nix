edaad466bbb97f66caaae6871fb0ffb45133dbe3
SiriusScaper <ruselmann.dev@gmail.com>
2024-05-12 14:53:08 -0700

    disabled caddy

--- modules/system/apps/jellyfin.nix
+++ modules/system/apps/jellyfin.nix
@@ -3,13 +3,13 @@
 {
   services.jellyfin.enable = true;
 
-  services.caddy = {
-      enable = true;
-      virtualHosts = {
-         "https://my-jellyfin.nixos.com".extraConfig = ''
-           reverse_proxy 127.0.0.1:8096
-           '';
-      };
-    };
+ # services.caddy = {
+  #    enable = true;
+   #   virtualHosts = {
+    #     "https://my-jellyfin.nixos.com".extraConfig = ''
+     #      reverse_proxy 127.0.0.1:8096
+      #     '';
+     # };
+   # };
 }
 
