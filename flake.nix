3c141fefcf5b2f3ce0f76ea5a796daf0e7160ff9
SiriusScaper <ruselmann.dev@gmail.com>
2024-05-23 10:08:46 -0700

    edits to flake.nix to fix issues with two hosts

--- flake.nix
+++ flake.nix
@@ -73,12 +73,12 @@
   {
 #     overlays.default = selfPkgs.overlay;
     nixosConfigurations = {
-      FarScape-One = nixpkgs.lib.nixosSystem {
+      FarScape-One = pkgs.lib.nixosSystem {
         inherit system;
         modules = [ (import ./hosts/desktop) ];
         specialArgs = { host="FarScape-One"; inherit self inputs username ; };
       };
-      tardis = nixpkgs.lib.nixosSystem {
+      tardis = pkgs.lib.nixosSystem {
         inherit system;
         modules = [ (import ./hosts/laptop) ];
         specialArgs = { host="tardis"; inherit self inputs username ; };
