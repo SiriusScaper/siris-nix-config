1c5229d2ecfab577a19c2ef1249cdd3421ca7407
SiriusScaper <ruselmann.dev@gmail.com>
2024-05-25 14:31:10 -0700

    updated nvidia.nix to source beta driver

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -12,6 +12,6 @@
    powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 }
