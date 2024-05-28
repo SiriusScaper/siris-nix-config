d9dd6652af262c30e9a364fd36be01e842984450
SiriusScaper <ruselmann.dev@gmail.com>
2024-05-28 13:23:05 -0700

    trying 6.8.10

--- modules/system/bootloader.nix
+++ modules/system/bootloader.nix
@@ -3,5 +3,5 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_8_10;
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_8;
 }
