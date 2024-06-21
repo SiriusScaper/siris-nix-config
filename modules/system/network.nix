bf536d86add0d627f792426ac7c513e6585836be
SiriusScaper <ruselmann.dev@gmail.com>
2024-06-21 16:10:37 -0700

    another attempt to get the wifi working

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -1,18 +1,20 @@
 { pkgs, config, lib, ... }: 
 {
-  networking = {
-    wireless.enable = true;
+  networking.wireless = {
+    enable = true;
     hostName = "tardis";
+    userControlled.enable = true;
+
   #  networkmanager.enable = true;
     #nameservers = [ "1.1.1.1" ];
-    firewall = {
-      enable = true;
-      allowedTCPPorts = [ 80 443 8096 ];
-      allowedUDPPorts = [ 59010 59011 8096 ];
+    #firewall = {
+      #enable = true;
+      #allowedTCPPorts = [ 80 443 8096 ];
+      #allowedUDPPorts = [ 59010 59011 8096 ];
       # allowedUDPPortRanges = [
         # { from = 4000; to = 4007; }
         # { from = 8000; to = 8010; }
       # ];
-    };
+    #};
   };
 }
