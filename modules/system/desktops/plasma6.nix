6e7cba6ed969eb0d3a25f941b2e94bb4a085a776
SiriusScaper <ruselmann.dev@gmail.com>
2024-06-29 19:50:10 -0700

    change: plasma6.nix simplified settings found on nixos discourse

--- modules/system/desktops/plasma6.nix
+++ modules/system/desktops/plasma6.nix
@@ -1,13 +1,25 @@
 { pkgs, config, lib, ... }:
 
 {
-  services.desktopManager.plasma6.enable = true;
+  services = {
+    desktopManager.plasma6 = {
+      enable = true;
+    };
+    displayManager = {
+      defaultSession = "plasma";
+      sddm = {
+        enable = true;
+        wayland.enable = true;
+      };
+    };
+  };
+#  .desktopManager.plasma6.enable = true;
   services.xserver.xkb = {
     layout = "us";
   #  variant = "";
    };
   services.libinput.enable = true;
-  services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.sddm.wayland.enable = false;
+ # services.xserver.enable = true;
+ # services.xserver.displayManager.sddm.enable = true;
+ # services.xserver.displayManager.sddm.wayland.enable = false;
 }
