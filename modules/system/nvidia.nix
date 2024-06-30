10e6ced63e34a801bbbfa2aa0ed64f99b6a7e933
SiriusScaper <ruselmann.dev@gmail.com>
2024-06-29 19:53:16 -0700

    change: nvidia.nix to disable finegrained to false after build fail

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -12,7 +12,7 @@
   hardware.nvidia = {
    modesetting.enable = true;
    powerManagement.enable = true;
-   powerManagement.finegrained = true;
+   powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.beta;
