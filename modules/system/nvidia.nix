be3cc1d8507bb433d00d8e20afbcf5a5fd71550b
SiriusScaper <ruselmann.dev@gmail.com>
2024-06-29 18:28:05 -0700

    change: nvidia drive to production

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -2,16 +2,16 @@
 
 {
 
-  boot.kernelParams = [ "nvidia-drm.modeset=1" "nvidia-drm.fndev=1" ];
+  boot.kernelParams = [ "nvidia-drm.modese=1" "nvidia-drm.fndev=1" ];
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"]; # or "nvidiaLegacy470 etc.
 
   hardware.nvidia = {
-    modesetting.enable = true;
+   modesetting.enable = true;
    powerManagement.enable = false;
    powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
   };
 }
