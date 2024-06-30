b9ce2eb51eabcbe693a9581719ebb07f142622a2
SiriusScaper <ruselmann.dev@gmail.com>
2024-06-29 19:27:24 -0700

    change: bootloader.nix to include nvidia specific parameters and nvidia.nix to disable boot parameters.

--- modules/system/bootloader.nix
+++ modules/system/bootloader.nix
@@ -1,5 +1,11 @@
 { pkgs, config, ... }:
 {
+  boot.kernelModules = [
+    "nvidia"
+    "nvidia_uvm"
+    "nvidia_modeset"
+    "nvidia_drm"
+  ];
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -1,15 +1,18 @@
 { pkgs, config, lib, ... }:
 
 {
+  hardware.opengl = {
+    enable = true;
+  };
 
-  boot.kernelParams = [ "nvidia-drm.modeset=1" "nvidia-drm.fbdev=1" ];
+  #boot.kernelParams = [ "nvidia-drm.modeset=1" "nvidia-drm.fbdev=1" ];
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"]; # or "nvidiaLegacy470 etc.
 
   hardware.nvidia = {
    modesetting.enable = true;
-   powerManagement.enable = false;
-   powerManagement.finegrained = false;
+   powerManagement.enable = true;
+   powerManagement.finegrained = true;
     open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.beta;
