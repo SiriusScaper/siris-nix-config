05d8977061db3515c48b20213839fb0dee45acfa
SiriusScaper <ruselmann.dev@gmail.com>
2024-06-30 18:38:43 -0700

    change: bootloader.nix switched back to zen kernel as it worked before

--- modules/system/bootloader.nix
+++ modules/system/bootloader.nix
@@ -9,5 +9,5 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
-  boot.kernelPackages = pkgs.linuxPackages_6_8;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 }
