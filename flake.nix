ed71ed4a4817e2076586a4a6da94f3d54a98e919
SiriusScaper <ruselmann.dev@gmail.com>
2024-07-12 14:37:52 -0700

    Syntax

--- flake.nix
+++ flake.nix
@@ -77,7 +77,7 @@
             nix.settings = {
               substituters = [ "https://cosmic.cachix.org/" ];
               trusted-public-keys = ["cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="];
-            }
+            };
           };
         (import ./hosts/laptop) 
         nixos-cosmic.nixosModules.default
