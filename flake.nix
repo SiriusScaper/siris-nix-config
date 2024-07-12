e1118a2399346a84257c31c6e4149532322985ff
SiriusScaper <ruselmann.dev@gmail.com>
2024-07-12 14:37:07 -0700

    Added public key for cosmic desktop

--- flake.nix
+++ flake.nix
@@ -76,7 +76,7 @@
           {
             nix.settings = {
               substituters = [ "https://cosmic.cachix.org/" ];
-              trusted-public-keys = [];
+              trusted-public-keys = ["cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="];
             }
           };
         (import ./hosts/laptop) 
