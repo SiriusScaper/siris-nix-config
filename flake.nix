e01aad6b218794048562f8732c900066bf6b36b2
SiriusScaper <ruselmann.dev@gmail.com>
2024-07-12 15:14:13 -0700

    Changes: flake.nix to try and enable cosmic desktop. Deleted commented code to clean up the file

--- flake.nix
+++ flake.nix
@@ -1,34 +1,3 @@
-# {
-#   description = "Nixos config flake";
-#
-#   inputs = {
-#     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-#     #nur.url = "github:nix-community/NUR";
-#
-#       home-manager = {
-#         url = "github:nix-community/home-manager";
-#         inputs.nixpkgs.follows = "nixpkgs";
-#       };
-#   };
-#
-#   outputs = { self, nixpkgs, ... }@inputs:
-#     let
-#       system = "x86_64-linux";
-#       pkgs = nixpkgs.legacyPackages.${system};
-#     in
-#     {
-#      nixosConfigurations.FarScape-One = nixpkgs.lib.nixosSystem {
-#       specialArgs = {inherit inputs;};
-#       modules = [
-#         ./hosts/homedesktop/configuration.nix
-#         inputs.home-manager.nixosModules.default
-#       ];
-#     };
-#   };
-# }
-
-{
-  description = "SiriusScaper's nixos configuration";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
