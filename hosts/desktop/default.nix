0367569899fb1a51e019be82cb35b2d9288dc44c
SiriusScaper <ruselmann.dev@gmail.com>
2024-07-17 16:49:29 -0700

    Edited: default.nix to delete commented code under desktop host

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,39 +1,6 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-/*
-{ config, pkgs, inputs, lib, ... }:
-
-{
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      inputs.home-manager.nixosModules.default
-    ];
-
-
-  # Set a Default DM
-  services.xserver.displayManager.defaultSession = "plasma";
-
-  
-  # Enable flatpak support
-  services.flatpak.enable = true;
-   
-
-
-  home-manager = {
-    # also pass inputs to home-manager modules
-    extraSpecialArgs = { inherit inputs; };
-    users = {
-      "sirius" = import ./home.nix;
-    };
-  };
-
-
-
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-}*/
-
 
 { pkgs, ... }:
 {
@@ -41,5 +8,5 @@
     ./hardware-configuration.nix
     ./../../modules/system
   ];
-#   powerManagement.cpuFreqGovernor = "performance";
+   powerManagement.cpuFreqGovernor = "performance";
 }
