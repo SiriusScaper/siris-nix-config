0400427f98537e15f621c0f875d9d33b19645e07
SiriusScaper <siriusscaper.dev@gmail.com>
2024-07-18 11:22:39 -0700

    Edit: going back to kernel 6.8 series

--- modules/system/bootloader.nix
+++ modules/system/bootloader.nix
@@ -3,6 +3,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
-  boot.kernelPackages = pkgs.linuxPackages;
+  boot.kernelPackages = pkgs.linuxPackages_6_8;
 #  boot.kernelParams = [ "nvidia-drm.modeset=1" "nvidia-drm.fndev=1" ];
 }
