61920099a87ebc371bbf2dcbdb4f7bf9178c611f
SiriusScaper <siriusscaper.dev@gmail.com>
2024-07-22 21:41:51 -0700

    Enable: mangohud in steam.nix

--- modules/system/apps/steam.nix
+++ modules/system/apps/steam.nix
@@ -2,10 +2,12 @@
 {
   programs.steam = {
     enable = true;
-    gamescopeSession.enable =true;
+    gamescopeSession.enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = false;
   };
+
+  environment.systemPackages = with pkgs; [ mangohud ];
 
    programs.gamemode.enable = true;
 
