afc290b4e43850a58a38af39dcd9d5bfc13b09bd
SiriusScaper <siriusscaper.dev@gmail.com>
2024-07-25 18:38:36 -0700

    Update: kernel to 6.10 or whatever the latest is

--- modules/system/bootloader.nix
+++ modules/system/bootloader.nix
@@ -3,6 +3,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
-  boot.kernelPackages = pkgs.linuxPackages_6_9;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 #  boot.kernelParams = [ "nvidia-drm.modeset=1" "nvidia-drm.fndev=1" ];
 }
